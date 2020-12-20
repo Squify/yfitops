9f3fd8d9aac8c913e16d82d4937cfbd076e0c002
MorcilloA <alexy.morcillo@gmail.com>
2020-12-20 16:07:33 +0100

    ajout du fichier des routes

---
+++ back/src/routes.js
@@ -0,0 +1,26 @@
+import {Router} from 'express';
+
+const router = Router();
+
+/**
+ * Routes User
+ */
+
+
+/**
+ * Routes Music
+ */
+
+
+/**
+ * Routes Playlist
+ */
+
+
+/**
+ * Routes Category
+ */
+
+
+
+export default router;
