ff1b8563bad038156b053735b39b6ddf7c5d0661
MorcilloA <alexy.morcillo@gmail.com>
2020-12-20 16:41:06 +0100

    CRUD User terminé

--- back/src/routes.js
+++ back/src/routes.js
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import UserController from './controllers/UserController';
 
 const router = Router();
 
@@ -6,6 +7,11 @@ const router = Router();
  * Routes User
  */
 
+router.get('/users', UserController.list);
+router.get('/users/:id', UserController.details);
+router.post('/users', UserController.store);
+router.put('/users/:id', UserController.update);
+router.delete('/users/:id', UserController.remove);
 
 /**
  * Routes Music
