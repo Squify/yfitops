b87a75d72bdc4ae46d47ffaa3bf06ad133e33a69
MorcilloA <alexy.morcillo@gmail.com>
2021-01-08 16:23:52 +0100

    affichage musiques d'une playlist

--- front/src/views/playlist/playlist.js
+++ front/src/views/playlist/playlist.js
@@ -1,9 +1,9 @@
 import './playlist.scss';
 import PlaylistService from '../../services/playlist.service';
 
-import React, {Component} from 'react';
-import {Button, Image, Table} from 'react-bootstrap';
-import {HiHeart} from 'react-icons/hi';
+import React, { Component } from 'react';
+import { Button, Image, Table } from 'react-bootstrap';
+import { HiHeart } from 'react-icons/hi';
 import MusicService from "../../services/music.service";
 
 export default class Playlist extends Component {
@@ -11,21 +11,33 @@ export default class Playlist extends Component {
     constructor(props) {
         super(props);
         this.state = ({
-            playlist: {
-                id: null,
-                name: null,
-                userId: null,
-                public: null,
-                musics: [],
-                image_path: null,
-            },
-            musics: []
+            image_path: null,
+            name: null,
+            musics: [],
+            isPublic: null,
+            userId: null
         });
     }
 
     async componentDidMount() {
         window.scrollTo(0, 0);
-        this.getPlaylist();
+
+        // let responseMusics = await 
+        let response = await PlaylistService.getPlaylist(this.props.match.params._id);
+        let playlist = response.data.playlist;
+        console.log(playlist);
+        let { name, musics, image_path, isPublic, userId } = playlist;
+        if (playlist) {
+            this.setState({
+                name,
+                isPublic,
+                image_path,
+                musics,
+                userId
+            });
+        } else
+            this.props.history.push('/');
+
     }
 
     componentDidUpdate(prevProps) {
@@ -35,58 +47,34 @@ export default class Playlist extends Component {
         }
     }
 
-    getPlaylist() {
-        let playlist = await PlaylistService.getPlaylist(this.props.match.params._id);
-        
-        if (playlist) {
-            this.setState({
-                playlist: {
-                    id: playlist._id,
-                    name: playlist.name,
-                    userId: playlist.userId,
-                    public: playlist.public,
-                    musics: playlist.musics,
-                    image_path: playlist.image_path,
-                }
-            });
-            this.getMusics(playlist);
-        } else
-            this.props.history.push('/');
-    }
-
-    getMusics(playlist) {
-        let musics = [];
-        playlist.musicsId.forEach(id => {
-            musics.push(MusicService.getMusics().find(music => music._id === id));
-        });
-        this.setState({musics: musics});
-    }
-
     render() {
+        let { name, isPublic, musics, image_path, userId } = this.state;
+        console.log(userId);
         return (
             <div className="global-container">
                 <div className="row">
                     <div className="col-3">
                         {
-                            this.state.playlist.image_path ?
-                                <Image src={`images/${this.state.playlist.image_path}`}
-                                       className="playlist-default playlist-cover"
-                                       alt="Playlist image"/>
+                            image_path ?
+                                <Image src={`${process.env.REACT_APP_HOST_API}/${image_path}`}
+                                    className="playlist-default playlist-cover"
+                                    alt="Playlist image" />
                                 :
                                 <Image src='images/playlist-default.png' className="playlist-default playlist-cover"
-                                       alt="Playlist image"/>
+                                    alt="Playlist image" />
                         }
                     </div>
                     <div className="col-7 playlist-test">
-                        <h5>Concocté par l'utilisateur #{this.state.playlist.userId}</h5>
-                        <h3>{this.state.playlist.name}</h3>
-                        <small>Playlist {this.state.playlist.public ? 'publique' : 'privée'}, {this.state.playlist.musicsId.length} titres
-                            ━ 4H30</small>
+                        {/* <h5>Concocté par {userId.firstname} {userId.lastname}</h5> */}
+                        <h3>{name}</h3>
+                        <small>Playlist {isPublic ? 'publique' : 'privée'}, {musics.length} titres
+                            {/* ━ 4H30 */}
+                        </small>
 
-                        <div className="header-buttons">
-                            {this.state.playlist.musicsId.length > 0 && <Button>Lire</Button>}
+                        {/* <div className="header-buttons">
+                            {musics.length > 0 && <Button>Lire</Button>}
                             <Button>Enregistrer</Button>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
 
@@ -94,31 +82,29 @@ export default class Playlist extends Component {
                     <div className="playlist-container">
                         <Table className="music-table" hover>
                             <thead>
-                            <tr>
-                                <th className="table-header"/>
-                                <th className="table-header">Titre</th>
-                                <th className="table-header">Artiste</th>
-                                <th className="table-header">Album</th>
-                                <th className="table-header">Durée</th>
-                            </tr>
+                                <tr>
+                                    <th className="table-header" />
+                                    <th className="table-header">Titre</th>
+                                    <th className="table-header">Artiste</th>
+                                    <th className="table-header">Durée</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            {
-                                this.state.musics.length > 0 ?
-                                    this.state.musics.map(function (music, i) {
-                                        return <tr className="music-tr">
-                                            <td><HiHeart/></td>
-                                            <td>{music.name}</td>
-                                            <td>{music.artiste}</td>
-                                            <td>{music.album}</td>
-                                            <td>{music.time}min</td>
+                                {
+                                    musics.length > 0 ?
+                                        musics.map((music, i) => {
+                                            return <tr key={i} className="music-tr">
+                                                <td><HiHeart /></td>
+                                                <td>{music.name}</td>
+                                                <td>{music.artist}</td>
+                                                <td>{music.duration}min</td>
+                                            </tr>
+                                        })
+                                        :
+                                        <tr className="music-tr">
+                                            <td colSpan="5">Aucune musique à afficher pour le moment</td>
                                         </tr>
-                                    })
-                                    :
-                                    <tr className="music-tr">
-                                        <td colSpan="5">Aucune musique à afficher pour le moment</td>
-                                    </tr>
-                            }
+                                }
                             </tbody>
                         </Table>
                     </div>
